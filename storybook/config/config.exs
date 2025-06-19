dbf0f2c9383420ac32d5ccb377aba670f0292b9c
Greg Woodward <woodward@users.noreply.github.com>
2025-06-18 17:01:45 -0700

    Update the esbuild version to 0.25.5 in config.exs
    Co-authored-by: brian.fischer@mechanical-orchard.com

--- storybook/config/config.exs
+++ storybook/config/config.exs
@@ -33,10 +33,10 @@ config :salad_storybook, SaladStorybook.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.17.11",
+  version: "0.25.5",
   default: [
     args:
-      ~w(js/app.js js/storybook.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js js/storybook.js --bundle --target=es2022 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
